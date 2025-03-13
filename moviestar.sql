e2f7c1c0db3fbbf89d683f40243d08ba30de9dbe
GleissonValentim <gleisson8452@hotmail.com>
2025-03-13 14:55:01 -0300

    Incluindo o arquivo de criação das tabelas do banco de dados.

---
+++ moviestar.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE moviestar;
+USE moviestar;
+
+CREATE TABLE users (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, name VARCHAR (100), lastname VARCHAR(100), email VARCHAR(200), password VARCHAR(200), image VARCHAR(200), token VARCHAR(200), bio TEXT);
+CREATE TABLE movies (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, title VARCHAR(100), description TEXT, image VARCHAR(200), trailer VARCHAR(150), category VARCHAR(50), length VARCHAR(45), users_id INT UNSIGNED, FOREIGN KEY(users_id) REFERENCES users(id));
+CREATE TABLE reviews (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, rating INT, review TEXT, users_id INT UNSIGNED, movies_id INT UNSIGNED, FOREIGN KEY(users_id) REFERENCES users(id), FOREIGN KEY(movies_id) REFERENCES movies(id));
